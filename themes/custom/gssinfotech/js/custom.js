128e71fd497a3bcba0cf20e0f167e53607105813
mohanagowri chowdam <mohanagowri.chowdam@GSSINFOTECH.LOCAL>
2019-01-24 16:10:27 +0530

    services inner pages and products and professional lists

--- themes/custom/gssinfotech/js/custom.js
+++ themes/custom/gssinfotech/js/custom.js
@@ -74,3 +74,41 @@ jQuery(document).ready(function($){
     });
 	jQuery('.sidemenufirst').appendTo('.region-left-sidebar');
 });
+
+(function ($) {
+	Drupal.behaviors.methodologyOwlcarousel = {
+		attach: function(context, settings) {
+			$('#methodology-owl').owlCarousel({
+				nav: true,
+				loop: true,
+				items: 1 ,
+				navText : ['<span class="arrow left"><div class="arrow-text"><</div></span>','<span class="arrow right"><div class="arrow-text">></div></span>'],
+				responsiveClass:true,
+				responsive:{
+				0:{
+					items:1,
+					nav:true
+				},
+				600:{
+					items:1,
+					nav:true
+				},
+				1000:{
+					items:1,
+					nav:true,
+				}
+			}
+			});
+			/*var myval = $("#owl-methodology .owl-item .item").attr('class').split(' ')[1];
+			var id = myval.substr( myval.indexOf ( '-' ) + 1 );
+			console.log(id);
+			$("#owl-methodology .owl-item .views-field-field-color-code .field-content").css('visibility', 'hidden');
+			$('#owl-methodology .owl-item.active .views-field-field-color-code .field-content').each(function() {
+				var colorcode = $.trim($(this).text());
+				$("#owl-methodology .owl-item.active").css("background-color", colorcode);
+			});	 */
+			//var colorcode = $.trim($('#owl-methodology .owl-item.active .views-field-field-color-code .field-content').text());
+			//$("#owl-methodology .owl-item.active").css("background-color", colorcode);
+		}
+	};
+}(jQuery));
